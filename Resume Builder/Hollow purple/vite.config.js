c71cb980640491701d6874b030dc45e91c3294e3
vedant05bhombe-netizen <vedant05bhombe@gmail.com>
2026-01-28 23:20:10 +0530

    one more commit

--- Resume Builder/Hollow purple/vite.config.js
+++ Resume Builder/Hollow purple/vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteSitemap } from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSitemap({
-      hostname: 'https://hollowpurplex.netlify.app/', // replace with your actual site URL
-    }),
-  ],
+   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5175,
-    allowedHosts: ['nonrefractive-else-lostly.ngrok-free.dev']
+     allowedHosts: ['nonrefractive-else-lostly.ngrok-free.dev']
+
+     
   },
 })
